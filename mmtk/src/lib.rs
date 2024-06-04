8b54fae638c97cc8427a89c4a06f2d2ea0cdfe3a
aninha623 <annabandrade@gmail.com>
2024-06-04 16:11:20 -0400

    modify lib.rs to compile with lazy_static

--- mmtk/src/lib.rs
+++ mmtk/src/lib.rs
@@ -2,11 +2,13 @@
 extern crate libc; //new
 extern crate mmtk;
 extern crate lazy_static; //new
+use lazy_static::lazy_static;
+
 
 //all new
 use mmtk::vm::VMBinding;
-//use mmtk::MMTKBuilder;
-//use mmtk::MMTK;
+use mmtk::MMTKBuilder; //used for lazy_static
+use mmtk::MMTK; //used for lazy_static
 
 pub mod active_plan; //new
 //pub mod api; //new, must stay commented out else compilation fails 6/4/2024
@@ -35,8 +37,15 @@ impl VMBinding for DummyVM {
 }
 
 //standard libraries
-//use std::sync::atomic::{AtomicBool, Ordering};
-//use std::sync::Mutex;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Mutex; //used for lazy_static
+
+
+//github to find initializeonce: mmtk-core/src/util/rust_util/mod.rs
+/*
+use mmtk::util::InitializeOnce;
+pub static SINGLETON: InitializeOnce<Box<MMTK<DummyVM>>> = InitializeOnce::new();
+
 
 #[no_mangle]
 pub extern "C" fn initialize_mmtk()
@@ -54,3 +63,17 @@ pub extern "C" fn initialize_mmtk()
     let _success2 = builder.options.plan.set(plan);
     let _mmtk = mmtk::memory_manager::mmtk_init::<DummyVM>(&builder);
 }
+*/
+pub static MMTK_INITIALIZED: AtomicBool = AtomicBool::new(false);
+
+lazy_static! {
+    pub static ref BUILDER: Mutex<MMTKBuilder> = Mutex::new(MMTKBuilder::new());
+    pub static ref SINGLETON: MMTK<DummyVM> = {
+        let builder = BUILDER.lock().unwrap();
+        debug_assert!(!MMTK_INITIALIZED.load(Ordering::SeqCst));
+        let ret = mmtk::memory_manager::mmtk_init(&builder);
+        MMTK_INITIALIZED.store(true, std::sync::atomic::Ordering::Relaxed);
+        *ret
+    };
+}
+
